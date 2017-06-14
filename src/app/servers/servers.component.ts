2487b576168339a44684507cd1948d540402e663
vvardhan <kvvardhanz@gmail.com>
2017-06-13 20:46:07 -0400

    2 way binding ex

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,17 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormsModule } from "@angular/forms";
+
 @Component({
   selector: 'app-servers',
-  template: `
-   <app-server></app-server>
-   <app-server></app-server>`,
+  //template: `
+   //<app-server></app-server>
+   //<app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = "No server was created! ";
+  serverName= 'testserver';
+
+  constructor() { 
+    setTimeout(() => {
+        this.allowNewServer = true;
+    }, 2000);
+  }
+
 
   ngOnInit() {
   }
 
+   onCreateServer() {
+       this.serverCreationStatus = 'Server was created! ';
+   }
+
+   onUpdateServerName(event: any ) {
+      this.serverName = (<HTMLInputElement>event.target).value;
+   }
 }
