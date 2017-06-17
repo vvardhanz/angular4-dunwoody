054d8eb83801b5b65192bc2f5d6c1d01987a8897
vvardhan <kvvardhanz@gmail.com>
2017-06-17 16:29:42 -0400

    Initial header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule } from '@angular/core'; 
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
