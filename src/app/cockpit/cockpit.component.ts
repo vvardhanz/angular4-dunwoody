cea6fe88fc912a741e65c0b97389e371aadfb7c4
vvardhan <kvvardhanz@gmail.com>
2017-06-24 20:51:11 -0400

    event emmiters @Input @Output

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
-   newServerName ='';
-   newServerComponent = '';
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  newServerName = '';
+  newServerContent = '';
 
   constructor() { }
 
@@ -15,19 +17,17 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer() {
-    this.serverElement.push({
-    	type: 'server',
-    	name: this.newServerName,
-    	content: this.newServerContent
-    });
+   this.serverCreated.emit({
+     serverName: this.newServerName,
+     serverContent: this.newServerContent
+   });
   }
 
   onAddBlueprint() {
-     this.serverElement.push({
-        type: 'blueprint',
-        name: this.newServerName,
-        content: this.newServerContent
-     });
+   this.blueprintCreated.emit({
+     serverName: this.newServerName,
+     serverContent: this.newServerContent
+   });
   }
 
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
 
-  element: {type: string , name: string, content: string};
+  @Input('srvElement') element: {type: string , name: string, content: string};
 
   constructor() { }
 
